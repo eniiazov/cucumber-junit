0863d0744b121de52e05b8b25e5c2687baf16a88
Cybertek Chicago <esen@cybertekschool.com>
2019-08-18 16:38:40 -0500

    jackson init

---
+++ src/test/java/test/Company.java
@@ -0,0 +1,32 @@
+package test;
+
+public class Company {
+
+    private String company;
+    private String title;
+    private Address address;
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
