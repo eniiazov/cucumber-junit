68b0aaf26449f9dc91fc931505c3dc5a9f10569e
Cybertek Chicago <esen@cybertekschool.com>
2019-08-25 12:18:46 -0500

    APIrunner class

--- src/test/java/steps/TeacherAPI_steps.java
+++ src/test/java/steps/TeacherAPI_steps.java
@@ -3,6 +3,7 @@ package steps;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import helper.HelperMethods;
 import io.cucumber.datatable.DataTable;
 import models.CustomResponse;
 import models.RequestBody;
@@ -17,10 +18,8 @@ public class TeacherAPI_steps {
     @Given("teacher is created with values")
     public void teacher_is_created_with_values(DataTable dataTable) {
 
-        requestBody = new RequestBody();
         Map<String, String> map = dataTable.asMap(String.class, String.class);
-
-
+        requestBody = HelperMethods.convertMapToObject(map);
 
     }
 
@@ -28,11 +27,14 @@ public class TeacherAPI_steps {
     public void user_hits_the_web_service(String string) {
         // Write code here that turns the phrase above into concrete actions
 
+
+
     }
 
     @Then("verify status code {string}")
     public void verify_status_code(String string) {
         // Write code here that turns the phrase above into concrete actions
+
 
     }
 
